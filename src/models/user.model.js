e76cb4429a8a0b5c01416a3ca7834740a9a31117
pradeepsahoo404 <sahoopradeep034@gmail.com>
2023-12-26 22:05:04 +0530

    video model and user model is completed with access toekn and refresh token method and pre middleware for password generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import Jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     userName : {
@@ -37,9 +39,49 @@ const userSchema = new mongoose.Schema({
     ],
     password : {
         type : String,
-        required: true,
+        required: [true , "password is required"]
+    },
+
+    refreshToken : {
+        type : String,
     }
 
 },{timestamps : true});
 
+userSchema.pre("save" ,  async function(next){
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password , 10)   
+    }
+    else 
+    return next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await  bcrypt.compare(password , this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+   return  Jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            userName : this.userName,
+            fullName :this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return Jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_SECRET
+        }
+    )
+}
 export const User = mongoose.model("User" , userSchema);
